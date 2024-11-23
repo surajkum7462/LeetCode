b90536fddb54f60a7b0e4931d91596b49e7dcc69
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2024-11-23 22:11:28 +0530

    Time: 3 ms (73.51%) | Memory: 48.4 MB (6.88%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,28 +1,64 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
-        int n=nums.length;
+        // List<Integer> ans = new ArrayList<>();
+        // int n=nums.length;
        
-        int cnt=0;
-        for(int i=0;i<n;i++)
-        {
-            cnt=0;
-            for(int j=i;j<n;j++)
-            {
-                if(nums[i]==nums[j])
-                {
-                    cnt++;                  
-                }
+        // int cnt=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     cnt=0;
+        //     for(int j=i;j<n;j++)
+        //     {
+        //         if(nums[i]==nums[j])
+        //         {
+        //             cnt++;                  
+        //         }
                
-            }
-             if(cnt>n/3 && !ans.contains(nums[i]))
-                {
-                        ans.add(nums[i]);
-                 }
+        //     }
+        //      if(cnt>n/3 && !ans.contains(nums[i]))
+        //         {
+        //                 ans.add(nums[i]);
+        //          }
                    
                
-        }
-        return ans; 
+        // }
+        // return ans; 
+
+        int n = nums.length; 
+
+int cnt1 = 0, cnt2 = 0; 
+int el1 = Integer.MIN_VALUE; 
+int el2 = Integer.MIN_VALUE; 
+
+for (int i = 0; i < n; i++) {
+    if (cnt1 == 0 && el2 != nums[i]) {
+        cnt1 = 1;
+        el1 = nums[i];
+    } else if (cnt2 == 0 && el1 != nums[i]) {
+        cnt2 = 1;
+        el2 = nums[i];
+    } else if (nums[i] == el1) cnt1++;
+    else if (nums[i] == el2) cnt2++;
+    else {
+        cnt1--; cnt2--;
+    }
+}
+
+List<Integer> ls = new ArrayList<>();
+
+cnt1 = 0; cnt2 = 0;
+for (int i = 0; i < n; i++) {
+    if (nums[i] == el1) cnt1++;
+    if (nums[i] == el2) cnt2++;
+}
+
+int mini = (int)(n / 3) + 1;
+if (cnt1 >= mini) ls.add(el1);
+if (cnt2 >= mini) ls.add(el2);
+
+
+
+        return ls;
         
     }
 }
