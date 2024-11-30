7ee76bc4135367ff6c70bf3179d83e508cb76b62
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2024-11-30 23:39:27 +0530

    Time: 28 ms (78.05%) | Memory: 51.6 MB (47.23%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,35 +1,70 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
 
-      List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(arr);
-        int n=arr.length;
-        for (int i = 0; i < n; i++) {
-            //remove duplicates:
-            if (i != 0 && arr[i] == arr[i - 1]) continue;
-
-            //moving 2 pointers:
-            int j = i + 1;
-            int k = n - 1;
-            while (j < k) {
-                int sum = arr[i] + arr[j] + arr[k];
-                if (sum < 0) {
+       // Optimal solution
+       Arrays.sort(arr);
+       List<List<Integer>> ans = new ArrayList<>();
+       int n=arr.length;
+       for(int i=0;i<n;i++)
+       {
+          if(i!=0 && arr[i]==arr[i-1])
+          {
+             continue;
+          }
+          int j=i+1;
+          int k= n-1;
+          while(j<k)
+          {
+            int sum = arr[i]+arr[j]+arr[k];
+            if(sum<0)
+            {
+                j++;
+            }
+            else if(sum>0)
+            {
+                k--;
+            }
+            else
+            {
+                List<Integer> list = Arrays.asList(arr[i],arr[j],arr[k]);
+                ans.add(list);
+                j++;
+                k--;
+                while(j<k && arr[j]==arr[j-1])
+                {
                     j++;
-                } else if (sum > 0) {
+                }
+                while(j<k && arr[k]==arr[k+1])
+                {
                     k--;
-                } else {
-                    List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
-                    ans.add(temp);
-                    j++;
-                    k--;
-                    //skip the duplicates:
-                    while (j < k && arr[j] == arr[j - 1]) j++;
-                    while (j < k && arr[k] == arr[k + 1]) k--;
                 }
             }
-        }
+          }
+       }
+       return ans;
 
-        return ans;
-        
+
+
+       // Better Solution
+        // Set<List<Integer>> st =new HashSet<>();
+        // for(int i=0;i<n;i++)
+        // {
+        //     Set<Integer> set = new HashSet<>();
+
+        //     for(int j=i+1;j<n;j++)
+        //     {
+        //         int third= -(arr[i]+arr[j]);
+
+        //         if(set.contains(third))
+        //         {
+        //             List<Integer> list = Arrays.asList(arr[i],arr[j],third);
+        //             list.sort(null);
+        //             st.add(list);
+        //         }
+        //         set.add(arr[j]);
+        //     }
+        // }
+        // List<List<Integer>> ans=new ArrayList<>(st);
+        // return ans;
     }
 }
