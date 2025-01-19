254886fcfc1d5e900b4d9e987e9e437bf1d35f7c
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-01-20 01:50:28 +0530

    Time: 1 ms (84.48%) | Memory: 41.2 MB (32.95%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+        
+        int sum=0;
+        
+        while(x!=0)
+        {
+            int r = x%10;
+            if(sum>Integer.MAX_VALUE/10 || sum<Integer.MIN_VALUE/10)
+            {
+                return 0;
+            }
+            sum=sum*10+r;
+            x=x/10;
+        }
+        
+        
+            return sum;
+        
+    }
+}
