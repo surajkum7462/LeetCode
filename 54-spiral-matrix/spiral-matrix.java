4aaf1463ddaa17e874f47bff68a7d3a6e345be22
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-01-28 20:59:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (42.32%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,41 +1,40 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-
-        List<Integer> list = new ArrayList<>();
-
+        List<Integer> ans = new ArrayList<>();
         int minCol=0;
         int maxCol=matrix[0].length-1;
         int minRow=0;
         int maxRow=matrix.length-1;
-        while(minRow<=maxRow && minCol<=maxCol)
+        while(minCol<=maxCol && minRow<=maxRow)
         {
             for(int i=minCol;i<=maxCol;i++)
             {
-                list.add(matrix[minRow][i]);
+                ans.add(matrix[minRow][i]);
             }
             minRow++;
             for(int i=minRow;i<=maxRow;i++)
             {
-                list.add(matrix[i][maxCol]);
+                ans.add(matrix[i][maxCol]);
             }
             maxCol--;
-            if(minRow<=maxRow){
-            for(int i=maxCol;i>=minCol;i--)
+            if(minRow<=maxRow)
             {
-                list.add(matrix[maxRow][i]);
+                for(int i=maxCol;i>=minCol;i--)
+                {
+                    ans.add(matrix[maxRow][i]);
+                }
+                maxRow--;
             }
-            maxRow--;
+             if(minCol<=maxCol)
+             {
+                for(int i=maxRow;i>=minRow;i--)
+                {
+                    ans.add(matrix[i][minCol]);
+                }
+                minCol++;
+             }
+           
         }
-       if(minCol<=maxCol)
-       {
-         for(int i=maxRow;i>=minRow;i--)
-         {
-            list.add(matrix[i][minCol]);
-         }
-         minCol++;
-       }
-
-        }
-        return list;
+       return ans;
     }
 }
