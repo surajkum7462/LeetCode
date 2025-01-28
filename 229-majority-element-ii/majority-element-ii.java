0360ca55a887c1b9adc50f360cadce1aedc9ad1b
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-01-28 23:08:56 +0530

    Time: 10 ms (45.43%) | Memory: 47.6 MB (86.74%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,64 +1,22 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        // List<Integer> ans = new ArrayList<>();
-        // int n=nums.length;
-       
-        // int cnt=0;
-        // for(int i=0;i<n;i++)
-        // {
-        //     cnt=0;
-        //     for(int j=i;j<n;j++)
-        //     {
-        //         if(nums[i]==nums[j])
-        //         {
-        //             cnt++;                  
-        //         }
-               
-        //     }
-        //      if(cnt>n/3 && !ans.contains(nums[i]))
-        //         {
-        //                 ans.add(nums[i]);
-        //          }
-                   
-               
-        // }
-        // return ans; 
+        List<Integer> ans = new ArrayList<>();
+        int n=nums.length;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int k:nums)
+        {
+            map.put(k,map.getOrDefault(k,0)+1);
+        }
 
-        int n = nums.length; 
-
-int cnt1 = 0, cnt2 = 0; 
-int el1 = Integer.MIN_VALUE; 
-int el2 = Integer.MIN_VALUE; 
-
-for (int i = 0; i < n; i++) {
-    if (cnt1 == 0 && el2 != nums[i]) {
-        cnt1 = 1;
-        el1 = nums[i];
-    } else if (cnt2 == 0 && el1 != nums[i]) {
-        cnt2 = 1;
-        el2 = nums[i];
-    } else if (nums[i] == el1) cnt1++;
-    else if (nums[i] == el2) cnt2++;
-    else {
-        cnt1--; cnt2--;
-    }
-}
-
-List<Integer> ls = new ArrayList<>();
-
-cnt1 = 0; cnt2 = 0;
-for (int i = 0; i < n; i++) {
-    if (nums[i] == el1) cnt1++;
-    if (nums[i] == el2) cnt2++;
-}
-
-int mini = (int)(n / 3) + 1;
-if (cnt1 >= mini) ls.add(el1);
-if (cnt2 >= mini) ls.add(el2);
-
-
-
-        return ls;
-        
+        for(Map.Entry<Integer,Integer> entry:map.entrySet())
+        {
+           int key = entry.getKey();
+           int value=entry.getValue();
+           if(value>(n/3))
+           {
+             ans.add(key);
+           }
+        }
+        return ans;
     }
 }
