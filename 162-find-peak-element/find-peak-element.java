42b17c3a17481a0f1abf5327df5481dcb266246b
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-02-08 02:11:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (31.37%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-
+        
         int l=0;
         int r=nums.length-1;
 
@@ -17,5 +17,6 @@ class Solution {
             }
         }
         return l;
+        
     }
 }
