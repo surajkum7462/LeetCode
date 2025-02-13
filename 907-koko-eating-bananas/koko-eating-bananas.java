a16b65856f1db2e663cd7045241f7e6283cd5492
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-02-13 11:34:51 +0530

    Time: 25 ms (45.23%) | Memory: 45.5 MB (23.70%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,16 +1,32 @@
 class Solution {
+    public int max(int[] arr)
+    {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++)
+        {
+            max=Math.max(arr[i],max);
+        }
+        return max;
+    }
+    public int findCeil(int[] arr,int hourly)
+    {
+        int totalHour=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            totalHour+=Math.ceil((double)(arr[i])/(double)(hourly));
+        }
+        return totalHour;
+    }
     public int minEatingSpeed(int[] piles, int h) {
-
         int l=1;
         int r=max(piles);
-        int ans=-1;
         while(l<=r)
         {
             int mid=(l+r)/2;
-            int totalHours=ceil(piles,mid);
-            if(totalHours<=h)
+            int totalHour=findCeil(piles,mid);
+            if(totalHour<=h)
             {
-                ans=mid;
                 r=mid-1;
             }
             else
@@ -18,29 +34,6 @@ class Solution {
                 l=mid+1;
             }
         }
-        return ans;
-    }
-    private int max(int[] arr)
-    {
-        int n=arr.length;
-        int max=arr[0];
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]>=max)
-            {
-                max=arr[i];
-            }
-        }
-        return max;
-    }
-    private int ceil(int[] piles,int h)
-    {
-        int totalH=0;
-        int n=piles.length;
-        for(int i=0;i<n;i++)
-        {
-            totalH+=Math.ceil((double)(piles[i])/(double)(h));
-        }
-        return totalH;
+        return l;
     }
 }
