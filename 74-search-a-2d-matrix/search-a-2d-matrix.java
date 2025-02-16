6dbbdc8900fffde92686b0ba2415d1d17a2a1e1a
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-02-16 15:35:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (38.51%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,52 +1,34 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-    //     int m=matrix.length;
-    //     int n=matrix[0].length;
-    //     for(int i=0;i<m;i++)
-    //     {
-    //         if(isPresent(matrix[i],target))
-    //         {
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
-    // public boolean isPresent(int[] arr,int x)
-    // {
-    //     int l=0;
-    //     int r=arr.length-1;
-    //     while(l<=r)
-    //     {
-    //         int mid=(l+r)/2;
-    //         if(arr[mid]==x)
-    //         {
-    //             return true;
-    //         }
-    //         else if(arr[mid]<x)
-    //         {
-    //             l=mid+1;
-    //         }
-    //         else
-    //         {
-    //             r=mid-1;
-    //         }
-    //     }
-    //     return false;
-
-     int n = matrix.length;
-        int m = matrix[0].length;
-
-        int low = 0, high = n * m - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int row = mid / m;
-            int col = mid % m;
-            if (matrix[row][col] == target) {
+    public boolean isPresent(int[] arr , int target)
+    {
+        int l=0;
+        int r=arr.length-1;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(arr[mid]==target)
+            {
                 return true;
-            } else if (matrix[row][col] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+            }
+            else if(arr[mid]<target)
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+            }
+        }
+        return false;
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        for(int i=0;i<matrix.length;i++)
+        {
+            if(isPresent(matrix[i],target)==true)
+
+            {
+                return true;
             }
         }
         return false;
