707c9b43ba138578e813628050198a26268c2c0c
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-02-16 12:44:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (53.37%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,50 +1,62 @@
 class Solution {
-    public int splitArray(int[] nums, int k) {
-         ArrayList<Integer> list = (ArrayList<Integer>) Arrays.stream(nums)
-                                                                  .boxed()
-                                                                  .collect(Collectors.toList());
-        return findPages(list,list.size(),k);
-    }
-    public static int findPages(ArrayList<Integer> arr, int n, int m) {
-        // Write your code here.
-        if(m>n)
-        {
-            return -1;
-        }
-        int l=Collections.max(arr);
-        int r=arr.stream().mapToInt(Integer::intValue).sum();
-
-        while(l<=r)
-        {
-            int mid=(l+r)/2;
-            int st=countSt(arr,mid);
-            if(st>m)
-            {
-                l=mid+1;
-            }
-            else{
-                r=mid-1;
-            }
-        }
-        return l;
-    }
-    public static int countSt(ArrayList<Integer> arr,int  pages)
+    public int sum(int[] arr)
     {
-        int n=arr.size();
-        int st=1;
-        long pgSt=0;
-        for(int i=0;i<n;i++)
+        int sum=0;
+        for(int n:arr)
         {
-            if(pgSt+arr.get(i)<=pages)
+            sum+=n;
+        }
+        return sum;
+    }
+    public int max(int[] arr)
+    {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++)
+        {
+            max=Math.max(arr[i],max);
+        }
+        return max;
+    }
+    public int count(int arr[] , int sum)
+    {
+        int sub=1;
+        int noSum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(noSum+arr[i]<=sum)
             {
-                pgSt+=arr.get(i);
+                noSum+=arr[i];
             }
             else
             {
-                st++;
-                pgSt=arr.get(i);
+                sub++;
+                noSum=arr[i];
             }
         }
-        return st;
+        return sub;
+    }
+    public int splitArray(int[] nums, int k) {
+        int low=max(nums);
+        int high=sum(nums);
+        if(nums.length<k)
+        {
+            return -1;
+        }
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int sum=count(nums,mid);
+            if(sum>k)
+            {
+                low=mid+1;
+
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        return low;
+        
     }
 }
