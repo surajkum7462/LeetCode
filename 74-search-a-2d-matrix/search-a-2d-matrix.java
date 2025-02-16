1235b3841d488f5c76354b92befdfc5b197deb78
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-02-16 15:40:32 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (80.62%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,36 +1,44 @@
 class Solution {
-    public boolean isPresent(int[] arr , int target)
-    {
-        int l=0;
-        int r=arr.length-1;
-        while(l<=r)
-        {
-            int mid=(l+r)/2;
-            if(arr[mid]==target)
-            {
-                return true;
-            }
-            else if(arr[mid]<target)
-            {
-                l=mid+1;
-            }
-            else
-            {
-                r=mid-1;
-            }
-        }
-        return false;
-    }
+    // public boolean isPresent(int[] arr , int target)
+    // {
+    //     int l=0;
+    //     int r=arr.length-1;
+    //     while(l<=r)
+    //     {
+    //         int mid=(l+r)/2;
+    //         if(arr[mid]==target)
+    //         {
+    //             return true;
+    //         }
+    //         else if(arr[mid]<target)
+    //         {
+    //             l=mid+1;
+    //         }
+    //         else
+    //         {
+    //             r=mid-1;
+    //         }
+    //     }
+    //     return false;
+    // }
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        for(int i=0;i<matrix.length;i++)
-        {
-            if(isPresent(matrix[i],target)==true)
+        int rows = matrix.length, cols = matrix[0].length;
+        int left = 0, right = rows * cols - 1; // Treat as 1D array
 
-            {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midElement = matrix[mid / cols][mid % cols]; // Convert 1D index to 2D
+
+            if (midElement == target) {
                 return true;
+            } else if (midElement < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
+
         return false;
     }
 }
