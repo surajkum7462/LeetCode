60b7b1ee6015100e948975f1cffd1c86d4052350
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-03-05 20:56:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (10.19%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,20 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-     double ans = 1.0;
-        long nn = n; // Use long to safely handle Integer.MIN_VALUE
-        if (nn < 0) {
-            nn = -nn; // Convert nn to positive
-            x = 1 / x; // Adjust x for negative exponent
-        }
+      if(n==0) return 1;
+      return powHelper(x,n);
+    }
+    public double powHelper(double x , int n)
+    {
+        if(n==0) return 1;
 
-        while (nn > 0) {
-            if (nn % 2 == 1) { // If nn is odd
-                ans *= x;
-            }
-            x *= x; // Square x
-            nn /= 2; // Halve nn
-        }
+        double half = powHelper(x,n/2);
 
-        return ans;
+        if(n%2==0)
+        {
+            return half*half;
+        }
+        else
+        {
+            return (n>0) ? half*half*x : half * half/x;
+        }
     }
 }
