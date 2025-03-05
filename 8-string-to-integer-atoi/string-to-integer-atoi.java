1ebaa56203649ea537176bdb7363737f7e6d0c4a
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-03-05 20:14:21 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (16.24%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,45 +1,47 @@
 class Solution {
     public int myAtoi(String s) {
+        if(s==null || s.isEmpty())
+        {
+            return 0;
 
-     if(s==null || s.isEmpty())
-     {
-        return 0;
-     }
-     s=s.trim();
-     if(s.isEmpty())
-     {
-        return 0;
-     }
-     int sign=+1;
-     long ans=0;
-     int i=0;
-     if(s.charAt(i)=='-')
-      {
-         sign=-1;
-         i++;
-      } 
+        }
+        s=s.trim();
+        if(s.isEmpty())
+        {
+            return 0;
+        }
+        int i=0;
+        int sign=1;
+
+        if(s.charAt(i)=='-')
+        {
+            sign=-1;
+            i++;
+        }
         else if(s.charAt(i)=='+')
-      {
-         sign=+1;
-         i++;
-      } 
-        
-
-      while(i<s.length() && Character.isDigit(s.charAt(i)))
-      {
-        char ch = s.charAt(i);
-         ans=ans*10+ch-'0';
-         if(sign==1 && ans>Integer.MAX_VALUE)
-         {
-            return Integer.MAX_VALUE;
-         }
-
-         if(sign==-1 && -ans<Integer.MIN_VALUE)
-         {
-            return Integer.MIN_VALUE;
-         }
-         i++;
-      }
-      return (int)(ans)*(sign);
+        {
+            sign=+1;
+            i++;
+        }
+        return atoiHelper(s,i,0,sign);
     }
+    public int atoiHelper(String s , int i, int ans , int sign)
+    {
+        if(i>=s.length() || !Character.isDigit(s.charAt(i)))
+        {
+            return (int)(ans*sign);
+        }
+       
+       int digit = s.charAt(i) - '0';
+
+        // Correct overflow check BEFORE updating ans
+        if (ans > (Integer.MAX_VALUE - digit) / 10) {
+            return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        }
+
+        ans = ans * 10 + digit;
+
+        return atoiHelper(s,i+1,ans,sign);
+    }
+
 }
