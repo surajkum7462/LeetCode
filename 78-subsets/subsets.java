c027b29884593ae4427096b8a7b3ff169f3c567a
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-03-06 20:58:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (46.38%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,40 +1,26 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-    //     List<List<Integer>> result = new ArrayList<>();
-    //     generateSubsets(nums, 0, new ArrayList<>(), result);
-    //     return result;
-    // }
 
-    // private void generateSubsets(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
-    //     if (index == nums.length) {
-    //         result.add(new ArrayList<>(current));
-    //         return;
-    //     }
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+       
+     
+        printSubset(nums,0,ans,temp);
+
+        return ans;
         
-    //     current.add(nums[index]);
-    //     generateSubsets(nums, index + 1, current, result);
-        
-    //     current.remove(current.size() - 1);
-    //     generateSubsets(nums, index + 1, current, result);
+    }
 
-      int n=nums.length;
-      int subsets=1<<n;
-      List<List<Integer>> ans = new ArrayList<>();
-
-      for(int num=0;num<=subsets-1;num++)
-      {
-         List<Integer> list = new ArrayList<>();
-          for(int i=0;i<=n-1;i++)
-          {
-            if((num & (1<<i))!=0)
-            {
-                list.add(nums[i]);
-            }
-          }
-          ans.add(list);
-
-      }
-      return ans;
-    
+    public void printSubset(int[] arr , int i , List<List<Integer>> ans, List<Integer> temp)
+    {
+       if(i>=arr.length)
+       {
+          ans.add(new ArrayList<>(temp));
+          return;
+       }
+       temp.add(arr[i]);
+       printSubset(arr,i+1,ans,temp);
+       temp.remove(temp.size()-1);
+       printSubset(arr,i+1,ans,temp);
     }
 }
