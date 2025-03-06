045fd6f61e3ee91b92c22d8a3f740659b4ccf2a1
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-03-06 23:47:21 +0530

    Time: 2 ms (99.66%) | Memory: 43.3 MB (52.42%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,27 +1,28 @@
 class Solution {
-    public void findCombination(int indx , int[] arr , int target , List<List<Integer>> ans , List<Integer> ds)
-    {   if(target==0)
+    public void combine(int[] arr , int indx , int target , List<List<Integer>> ans , List<Integer> ds)
+    {
+
+            if(target==0)
             {
                 ans.add(new ArrayList<>(ds));
             }
-            
-        
-        for(int i=indx;i<arr.length;i++){
+
+
+        for(int i=indx;i<arr.length;i++)
+        {
             if(i>indx && arr[i]==arr[i-1]) continue;
             if(arr[i]>target) break;
             ds.add(arr[i]);
-            findCombination(i+1,arr,target-arr[i],ans,ds);
+            combine(arr,i+1,target-arr[i],ans,ds);
             ds.remove(ds.size()-1);
         }
-
-       
     }
-
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-         List<List<Integer>> ans = new ArrayList<>();
-         Arrays.sort(candidates);
-        findCombination(0,candidates,target,ans,new ArrayList<>());
-
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        Arrays.sort(candidates);
+        combine(candidates,0,target,ans,temp);
         return ans;
+        
     }
 }
