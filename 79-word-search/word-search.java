0f047494e646dd5c9080f5b201aa55cde46b07b6
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-03-17 21:15:28 +0530

    Time: 135 ms (77.10%) | Memory: 41.8 MB (38.85%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,51 +1,54 @@
 class Solution {
+    
     public boolean exist(char[][] board, String word) {
-        int m=board.length;
-        int n=board[0].length;
-        for(int r=0;r<m;r++)
+        int r = board.length;
+        int c=board[0].length;
+        for(int i=0;i<r;i++)
         {
-            for(int c=0;c<n;c++)
+            for(int j=0;j<c;j++)
             {
-                if(word.charAt(0)==board[r][c])
+                if(word.charAt(0)==board[i][j])
                 {
-                  boolean found=  dfs(board,r,c,word,0);
-                  if(found) return true;
+                    boolean found = dfs(board,i,j,word,0);
+                    if(found)
+                    {
+                        return true;
+                    }
                 }
             }
         }
-        return false;
+      return false;
+        
     }
 
-    public boolean dfs(char[][] board , int r,int c , String word,int wordIndex)
+    public boolean dfs(char[][] board , int r , int c , String word ,int wordIndx)
     {
-        // base cases
-        if(wordIndex==word.length())
+        if(wordIndx==word.length())
         {
             return true;
-
         }
-        // out of bound case
+
         int m=board.length;
         int n=board[0].length;
+
         if(r<0 || c<0 || r>=m || c>=n)
         {
             return false;
         }
-        //invalid cases
-        if(board[r][c]==' ' || board[r][c]!=word.charAt(wordIndex))
+
+        if(board[r][c]==' ' || board[r][c]!=word.charAt(wordIndx))
         {
             return false;
         }
-        // mark as visited
-        char ch=board[r][c];
+        char ch = board[r][c];
         board[r][c]=' ';
 
-        if( dfs(board,r-1,c,word,wordIndex+1) || dfs(board,r,c+1,word,wordIndex+1) 
-        || dfs(board,r+1,c,word,wordIndex+1) || dfs(board,r,c-1,word,wordIndex+1))
+        if(dfs(board,r-1,c,word,wordIndx+1) || dfs(board,r+1,c,word,wordIndx+1) ||
+        dfs(board,r,c-1,word,wordIndx+1) || dfs(board,r,c+1,word,wordIndx+1))
         {
             return true;
         }
-        board[r][c]=ch;
-        return false;
+       board[r][c]=ch;
+       return false;
     }
 }
