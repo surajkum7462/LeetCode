ce989278cdda2cb0f03adb477756fab9a4f847f9
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-04-03 21:47:42 +0530

    Time: 119 ms (9.92%) | Memory: 60.4 MB (8.31%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,38 +1,50 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
+        int n=heights.length;
         int[] left = new int[n];
         int[] right = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        
-        // Find the nearest smaller element to the left
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
-                stack.pop();
+        left = findpse(heights);
+        right=findnse(heights);
+        int maxi=0;
+        for(int i=0;i<heights.length;i++)
+        {
+            int width=right[i]-left[i]-1;
+            int area=heights[i]*width;
+            maxi=Math.max(area,maxi);
+        }
+        return maxi;
+    }
+    public int[] findpse(int[] arr)
+    {
+        int[] res = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i])
+            {
+               st.pop();
             }
-            left[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(i);
+            res[i]=st.isEmpty()?-1:st.peek();
+            st.push(i);
+
         }
-        
-        stack.clear();
-        
-        // Find the nearest smaller element to the right
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
-                stack.pop();
+        return res;
+    }
+
+     public int[] findnse(int[] arr)
+    {
+        int[] res = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i])
+            {
+               st.pop();
             }
-            right[i] = stack.isEmpty() ? n : stack.peek();
-            stack.push(i);
+            res[i]=st.isEmpty()?arr.length:st.peek();
+            st.push(i);
+
         }
-        
-        // Calculate the maximum area
-        int maxArea = 0;
-        for (int i = 0; i < n; i++) {
-            int width = right[i] - left[i] - 1;
-            int area = heights[i] * width;
-            maxArea = Math.max(maxArea, area);
-        }
-        
-        return maxArea;
+        return res;
     }
 }
