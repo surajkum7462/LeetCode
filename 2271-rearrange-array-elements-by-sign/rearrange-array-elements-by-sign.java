ea1ac17aab6851acb1918fa32d8008c593981cfd
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-05-13 20:04:03 +0530

    Time: 3 ms (100.00%) | Memory: 76.8 MB (82.82%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,34 +1,22 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int k=0;
-        int l=0;
         int n=nums.length;
-        int[] pos = new int[n/2];
-        int[] neg = new int[n/2];
-        for(int i=0;i<nums.length;i++)
+        int poIndx=0;
+        int negIndx=1;
+        int[] ans = new int[n];
+        for(int num:nums)
         {
-            if(nums[i]>0)
+            if(num>0)
             {
-                pos[k++]=nums[i];
+                ans[poIndx]=num;
+                poIndx+=2;
             }
             else
             {
-                neg[l++]=nums[i];
+                ans[negIndx]=num;
+                negIndx+=2;
             }
         }
-        k=0;
-        l=0;
-        for(int i=0;i<nums.length;i++)
-        {
-           if(i%2==0)
-           {
-             nums[i]=pos[k++];
-           }
-           else
-           {
-             nums[i]=neg[l++];
-           }
-        }
-        return nums;
+        return ans;
     }
 }
