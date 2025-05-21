2c684daac88bb0a3e3e08f07b976aa3dfaaa3a7f
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-05-21 10:30:55 +0530

    Time: 164 ms (8.08%) | Memory: 45.6 MB (11.69%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,43 +1,38 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);
-        int n = nums.length;
-        if (n == 0 || n < 4) {
-            return ans;
+        int n=nums.length;
+        if(nums==null || n<4)
+        {
+            return new ArrayList<>();
         }
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            for (int j = i + 1; j < n; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1])
-                    continue;
-                int k = j + 1;
-                int l = n - 1;
-                while (k < l) {
-                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
-                    if (sum == target) {
-                        List<Integer> list = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
-                        ans.add(list);
-                        l--;
-                        k++;
-
-                        while (k < l && nums[k] == nums[k - 1])
-                            k++;
-                        while (k < l && nums[l] == nums[l + 1])
-                            l--;
-
-                    } else if (sum < target) {
-                        k++;
-
-                    } else {
-                        l--;
+        Set<List<Integer>> ans = new HashSet<>();
+        Arrays.sort(nums);
+        for(int i=0;i<n-3;i++)
+        {
+            for(int j=i+1;j<n-2;j++)
+            {
+                int left=j+1;
+                int right=n-1;
+                while(left<right)
+                {
+                    long sum=(long) nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum==target)
+                    {
+                        ans.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                        left++;
+                        right--;
+                    }
+                    else if(sum<target)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
                     }
                 }
-
             }
-
         }
-        return ans;
+     return new ArrayList<>(ans);
     }
 }
