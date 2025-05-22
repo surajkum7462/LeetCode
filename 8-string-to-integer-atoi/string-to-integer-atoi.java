41192776326c0f1efe1e11f4116bb2c4166a4cd6
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-05-22 13:15:45 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (11.32%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,46 +1,38 @@
 class Solution {
     public int myAtoi(String s) {
-        if(s==null || s.isEmpty())
-        {
-            return 0;
-
-        }
-        s=s.trim();
-        if(s.isEmpty())
-        {
-            return 0;
-        }
-        int i=0;
-        int sign=1;
-
-        if(s.charAt(i)=='-')
-        {
-            sign=-1;
-            i++;
-        }
-        else if(s.charAt(i)=='+')
-        {
-            sign=+1;
-            i++;
-        }
+       if(s==null ||s.length()==0)
+       {
+        return 0;
+       }
+       s=s.trim();
+        if (s.length() == 0) return 0;
+       int sign=1;
+       int i=0;
+       if(s.charAt(i)=='-')
+       {
+          sign=-1;
+          i++;
+       }else if(s.charAt(i)=='+')
+       {
+         sign=+1;
+         i++;
+       }
         return atoiHelper(s,i,0,sign);
     }
-    public int atoiHelper(String s , int i, int ans , int sign)
+
+    public int atoiHelper(String s,int i,int ans,int sign)
     {
         if(i>=s.length() || !Character.isDigit(s.charAt(i)))
         {
             return (int)(ans*sign);
         }
-       
-       int digit = s.charAt(i) - '0';
 
-        // Correct overflow check BEFORE updating ans
-        if (ans > (Integer.MAX_VALUE - digit) / 10) {
-            return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        int digit = s.charAt(i)-'0';
+        if(ans>(Integer.MAX_VALUE-digit)/10)
+        {
+            return sign==1?Integer.MAX_VALUE:Integer.MIN_VALUE;
         }
-
-        ans = ans * 10 + digit;
-
+        ans=ans*10+digit;
         return atoiHelper(s,i+1,ans,sign);
     }
 
