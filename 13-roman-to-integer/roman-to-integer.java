23dcd30a52cd70d4e3e83890970b23e612a7b376
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-05-22 12:52:54 +0530

    Time: 3 ms (79.96%) | Memory: 44.7 MB (69.07%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,34 +1,42 @@
 class Solution {
     public int romanToInt(String s) {
-        int sum=0;
-        int n=s.length();
-        for(int i=0;i<s.length();i++)
-        {
-            char ch  = s.charAt(i);
-            if((i+1)<n && getVal(ch)<getVal(s.charAt(i+1)))
-            {
-                sum-=getVal(ch);
-            }
-            else
-            {
-                sum+=getVal(ch);
-            }
-        }
-        return sum;
+       
+       int sum=0;
+       int n=s.length();
+       for(int i=0;i<n;i++)
+       {
+          char ch = s.charAt(i);
+          if((i+1)<n && getVal(ch)<getVal(s.charAt(i+1)))
+          {
+             sum-=getVal(ch);
+          }
+          else{
+            sum+=getVal(ch);
+          }
+       }
+
+       return sum;
     }
 
     public int getVal(char ch)
     {
         switch(ch)
         {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
+            case 'I':return 1;
+                   
+            case 'V':return 5;
+                   
+            case 'X':return 10;
+                  
+            case 'L':return 50;
+                  
+            case 'C':return 100;
+                   
+            case 'D':return 500;
+                  
             case 'M':return 1000;
             default:return 0;
+
         }
     }
 }
