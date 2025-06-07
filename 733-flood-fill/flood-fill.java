c55f199d2691027b5d150ca2823d052c66834754
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2025-06-07 13:30:24 +0530

    Time: 1 ms (48.97%) | Memory: 44.9 MB (69.25%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,32 +1,55 @@
 class Solution {
-     private void dfs(int row, int col, 
-     int[][] ans,
-     int[][] image, 
-     int newColor, int delRow[], int delCol[],
-     int iniColor) {
-        // color with new color
-        ans[row][col] = newColor; 
-        int n = image.length;
-        int m = image[0].length; 
-        // there are exactly 4 neighbours
-        for(int i = 0;i<4;i++) {
-            int nrow = row + delRow[i]; 
-            int ncol = col + delCol[i]; 
-            // check for valid coordinate 
-            // then check for same initial color and unvisited pixel
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol < m && 
-            image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor) {
-                dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor); 
-            }
-        }
-    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int iniColor = image[sr][sc]; 
-        int[][] ans = image; 
-        // delta row and delta column for neighbours
-        int delRow[] = {-1, 0, +1, 0};
-        int delCol[] = {0, +1, 0, -1}; 
-        dfs(sr, sc, ans, image, color, delRow, delCol, iniColor); 
-        return ans;  
+
+         if (image == null || image.length == 0) return image;
+
+        Queue<int[]> q = new LinkedList<>();
+        int row = image.length;
+        int col = image[0].length;
+       
+       int originalColor = image[sr][sc];
+       if(originalColor==color)
+       {
+         return image;
+       }
+
+      q.offer(new int[]{sr,sc});
+      image[sr][sc]=color;
+        
+
+       
+
+       
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        // Step 2: BFS traversal
+        while (!q.isEmpty()) {
+           
+            
+
+           
+                int[] point = q.poll();
+
+                for (int j = 0; j < 4; j++) {
+                    int x = point[0] + dx[j];
+                    int y = point[1] + dy[j];
+
+                    if (x < 0 || y < 0 || x >= row || y >= col || image[x][y] != originalColor) {
+                        continue;
+                    }
+
+                    image[x][y] = color;
+                   
+                    q.offer(new int[]{x, y});
+                    
+                }
+            
+
+           
+        }
+
+        return image;
+        
     }
 }
