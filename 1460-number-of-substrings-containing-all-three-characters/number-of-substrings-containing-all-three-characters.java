acb94a467bd1d53b840043ec16ca432d5d974ffb
Suraj Kumar <162699747+surajkum7462@users.noreply.github.com>
2025-07-23 21:11:00 +0530

    Time: 11 ms (89.23%) | Memory: 45 MB (22.15%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,21 +1,21 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-          int[] lastSeen = {-1, -1, -1}; // Tracks the last seen indices of 'a', 'b', 'c'
-        int result = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            // Update the last seen index of the current character
-            lastSeen[s.charAt(i) - 'a'] = i;
-            
-            // Find the smallest index among the last seen indices
-            int earliestLastSeen = Math.min(lastSeen[0], Math.min(lastSeen[1], lastSeen[2]));
-            
-            // If all characters have been seen at least once
-            if (earliestLastSeen != -1) {
-                result += earliestLastSeen + 1;
+
+        int[] lastSeen = {-1,-1,-1};
+        int result=0;
+        for(int i=0;i<s.length();i++)
+        {
+            lastSeen[s.charAt(i)-'a']=i;
+
+
+            int earliestSeen  = Math.min(lastSeen[0],Math.min(lastSeen[1],lastSeen[2]));
+
+            if(earliestSeen!=-1)
+            {
+                result+=earliestSeen+1;
             }
         }
-        
         return result;
+        
     }
 }
