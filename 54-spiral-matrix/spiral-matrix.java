e8b239848b9e6f2f08e27b077f01291d64f7354c
Suraj Kumar <162699747+surajkum7462@users.noreply.github.com>
2025-08-03 11:43:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (42.51%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,41 +1,41 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int minCol=0;
-        int maxCol=n-1;
-        int minRow=0;
-        int maxRow=m-1;
-        List<Integer> ans = new ArrayList<>();
-        while(minCol<=maxCol && minRow<=maxRow)
-        {
-           for(int i=minCol;i<=maxCol;i++)
-           {
-              ans.add(matrix[minRow][i]);
-           }
-           minRow++;
-           for(int i=minRow;i<=maxRow;i++)
-           {
-              ans.add(matrix[i][maxCol]);
-           }
-           maxCol--;
-           if(minRow<=maxRow)
-           {
+       int m=matrix.length;
+       int n=matrix[0].length;
+       int minCol=0;
+       int maxCol=n-1;
+       int minRow=0;
+       int maxRow=m-1;
+       List<Integer> ans = new ArrayList<>();
+       while(minCol<=maxCol && minRow<=maxRow)
+       {
+          for(int i=minCol;i<=maxCol;i++)
+          {
+             ans.add(matrix[minRow][i]);
+          }
+          minRow++;
+          for(int i=minRow;i<=maxRow;i++)
+          {
+             ans.add(matrix[i][maxCol]);
+          }
+          maxCol--;
+          if(minRow<=maxRow)
+          {
              for(int i=maxCol;i>=minCol;i--)
              {
-                 ans.add(matrix[maxRow][i]);
+                ans.add(matrix[maxRow][i]);
              }
              maxRow--;
-           }
-           if(minCol<=maxCol)
-           {
-             for(int i=maxRow;i>=minRow;i--)
-             {
-                 ans.add(matrix[i][minCol]);
-             }
-             minCol++;
-           }
-        }
-        return ans;
+          }
+          if(minCol<=maxCol)
+          {
+            for(int i=maxRow;i>=minRow;i--)
+            {
+                ans.add(matrix[i][minCol]);
+            }
+            minCol++;
+          }
+       }
+       return ans;
     }
 }
