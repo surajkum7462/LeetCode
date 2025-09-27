ff48526fe82242240c71c4315b09c865652d2051
Suraj Kumar <162699747+surajkum7462@users.noreply.github.com>
2025-09-27 22:47:53 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (64.48%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,38 +1,54 @@
 class Solution {
     public int myAtoi(String s) {
-        if(s.length()==0 || s==null)
+
+        if(s==null)
         {
             return 0;
         }
+
         s=s.trim();
-        if(s.length()==0) return 0;
+
+        if(s.length()==0)
+        {
+            return 0;
+        }
 
         int i=0;
-        int sign=+1;
-        if(s.charAt(i)=='-')
-        {
-            sign=-1;
-            i++;
-        }else if(s.charAt(i)=='+')
+        int sign=1;
+
+        if(s.charAt(i)=='+')
         {
             sign=+1;
-            i++;
+             i++;
+          
         }
+        else if(s.charAt(i)=='-')
+        {
+             sign=-1;
+              i++;
+             
+        }
+       
         return atoiHelper(s,i,0,sign);
+        
+
     }
 
-    public int atoiHelper(String s , int i , int ans , int sign)
+    public int atoiHelper(String s,int i,int ans,int sign)
     {
-        if(i>=s.length() || !Character.isDigit(s.charAt(i)))
-        {
-            return (int)(ans*sign);
-        }
+
+         if(i>=s.length() || !Character.isDigit(s.charAt(i)))
+         {
+             return (int)(sign*ans);
+         }
+
         int digit = s.charAt(i)-'0';
         if(ans>(Integer.MAX_VALUE-digit)/10)
         {
             return sign==1?Integer.MAX_VALUE:Integer.MIN_VALUE;
         }
-        ans = ans*10+digit;
+        ans=ans*10+digit;
+
         return atoiHelper(s,i+1,ans,sign);
     }
 
