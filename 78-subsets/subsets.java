b4b840e9f27d4b78ba7700e352cfab37ea3c480b
Suraj Kumar <162699747+surajkum7462@users.noreply.github.com>
2025-10-07 12:07:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (47.99%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,22 +1,24 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
+      List<List<Integer>> ans  = new ArrayList<>();
+      List<Integer> temp = new ArrayList<>();
 
-        printSub(nums,0,ans,temp);
-        return ans;
+      generateSubsets(nums,0,ans,temp);
+
+      return ans;
     }
-    public void printSub(int[] arr , int i ,  List<List<Integer>> ans , List<Integer> temp)
+
+    public void generateSubsets(int[] nums,int i,List<List<Integer>> ans , List<Integer> temp)
     {
-        if(i>=arr.length)
+        if(i>=nums.length)
         {
             ans.add(new ArrayList<>(temp));
             return;
         }
-        temp.add(arr[i]);
-        printSub(arr,i+1,ans,temp);
-        temp.remove(temp.size()-1);
-        printSub(arr,i+1,ans,temp);
 
+        temp.add(nums[i]);
+        generateSubsets(nums,i+1,ans,temp);
+        temp.remove(temp.size()-1);
+         generateSubsets(nums,i+1,ans,temp);
     }
 }
