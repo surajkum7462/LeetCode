7a3d43dc5a8ed4e6037ba906395fe5ce4f966408
Suraj Kumar <162699747+surajkum7462@users.noreply.github.com>
2025-10-08 11:05:47 +0530

    Time: 3 ms (71.33%) | Memory: 43.5 MB (23.66%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,28 +1,39 @@
 class Solution {
-    public void combine(int[] arr , int indx , int target , List<List<Integer>> ans , List<Integer> ds)
-    {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+       List<List<Integer>> ans = new ArrayList<>();
+       List<Integer> temp  = new ArrayList<>();
 
-            if(target==0)
+       Arrays.sort(candidates);
+
+       helpCombination(candidates,0,ans,temp,target);
+       return ans;
+    }
+
+    public void helpCombination(int[] nums , int start ,  List<List<Integer>> ans , List<Integer> temp , int target)
+    {
+        if(target==0)
+        {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int i=start;i<nums.length;i++)
+        {
+            if(i>start && nums[i]==nums[i-1])
             {
-                ans.add(new ArrayList<>(ds));
+                continue;
             }
 
+            if(nums[i]>target)
+            {
+                break;
+            }
 
-        for(int i=indx;i<arr.length;i++)
-        {
-            if(i>indx && arr[i]==arr[i-1]) continue;
-            if(arr[i]>target) break;
-            ds.add(arr[i]);
-            combine(arr,i+1,target-arr[i],ans,ds);
-            ds.remove(ds.size()-1);
+            temp.add(nums[i]);
+
+            helpCombination(nums,i+1,ans,temp,target-nums[i]);
+
+            temp.remove(temp.size()-1);
         }
-    }
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        Arrays.sort(candidates);
-        combine(candidates,0,target,ans,temp);
-        return ans;
-        
     }
 }
