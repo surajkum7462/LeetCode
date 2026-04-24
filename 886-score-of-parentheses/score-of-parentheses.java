a3bfc0a85091faa286df48b5d9df20868de89caf
Suraj Kumar <162699747+surajkum7462@users.noreply.github.com>
2026-04-24 11:00:50 +0530

    Time: 1 ms (56.96%) | Memory: 42.9 MB (14.47%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.java
+++ 886-score-of-parentheses/score-of-parentheses.java
@@ -1,28 +1,28 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        Stack<Integer> st = new Stack<>();
-        for(char ch:s.toCharArray())
-        {
-            if(ch=='(')
-            {
-                st.push(0);
-            }
-            else
-            {
-                int val=0;
-                while(st.peek()!=0)
-                {
-                    val+=st.pop();
-                }
-                st.pop();
-                st.push(val==0?1:2*val);
-            }
-        }
-        int total=0;
-        while(!st.isEmpty())
-        {
-            total+=st.pop();
-        }
-        return total;
+       Stack<Integer> st = new Stack<>();
+       for(char ch : s.toCharArray())
+       {
+          if(ch == '(')
+          {
+             st.push(0);
+          }
+          else
+          {
+             int val = 0;
+             while(st.peek()!=0)
+             {
+                val+=st.pop();
+             }
+             st.pop();
+             st.push(val==0?1:2*val);
+          }
+       }
+       int total = 0;
+       while(!st.isEmpty())
+       {
+         total+=st.pop();
+       }
+       return total;
     }
 }
